15c86611b80b1a33201ae00f08b1a60d953f1460
arseniit <arseniit@gmail.com>
2024-01-06 00:07:04 +0100

    Realize quest 08 based on Fly interface

---
+++ src/main/java/org/example/wcs/java/_08/Fly.java
@@ -0,0 +1,14 @@
+package org.example.wcs.java._08;
+
+public interface Fly {
+
+    void takeOff();
+
+    int ascend(int meters);
+
+    int glide();
+
+    int descend(int meters);
+
+    void land();
+}
